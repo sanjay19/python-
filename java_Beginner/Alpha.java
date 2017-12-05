382155e31db4307b9c0c325dc8849ec5e04a013d
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2017-12-05 16:27:43 +0530

    Update Alpha.java

--- java_Beginner/Alpha.java
+++ java_Beginner/Alpha.java
@@ -10,8 +10,8 @@ public class Alpha {
         c=sc.next().charAt(0);
        
        if((c >='a' && c<='z') ||(c>='A' && c<='Z'))
-           System.out.println("albhabet");
+           System.out.println("alphabet");
        else
-           System.out.println("not albhabet");
+           System.out.println("not alphabet");
     }
 }
