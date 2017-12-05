a5ef37771afbf95d1317307ffe09e1bf7fe953bd
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2017-12-05 14:27:37 +0530

    Create positive.java

---
+++ java_Beginner/positive.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+public class Positive {
+
+
+    
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("enter the value:");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n<0)
+    {
+        System.out.println("negative");
+    }
+    
+    
+        if(n>0)
+        {
+    
+        System.out.println("positve");
+        }
+    if(n==0)
+        
+        {
+        System.out.println("zero");
+    }
+       
+        
+}
+}
