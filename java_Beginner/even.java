f8bb9eac008133e66a41a327cd8405d8d75a101f
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2017-12-05 15:19:15 +0530

    Create even.java

---
+++ java_Beginner/even.java
@@ -0,0 +1,21 @@
+
+
+
+import java.util.*;
+import java.lang.*;
+public class Even {
+
+    public static void main(String[] args) {
+        int n;
+        System.out.println("");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==0)
+            System.out.println("even");
+        else 
+            System.out.println("odd");
+        
+        
+        
+    }
+}
