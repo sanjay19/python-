0261c532b6f3f5cb8b77dcfce742bb9f753652d3
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2017-12-05 15:56:12 +0530

    Create Vowel.java

---
+++ java_Beginner/Vowel.java
@@ -0,0 +1,34 @@
+
+package javaapplication35;
+import java.util.*;
+import java.lang.*;
+public class Vowel {
+
+   
+    public static void main(String[] args) {
+        int i=0;
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        switch(ch)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+                    case 'o':
+                    case 'u':
+                    case 'A':
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                        i++;}
+        if(i==1)
+            System.out.println("vowel");
+        else
+            System.out.println("not vowel");
+                       
+                        
+        } 
+}
+    
+     
