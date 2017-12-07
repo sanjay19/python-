f84c2b72425f4f3b91f14b3b962cee3ecc3cf7f1
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2017-12-07 20:00:04 +0530

    Create Hello.java

---
+++ java_Beginner/Hello.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import java.lang.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        for(int i=0;i<5;i++)
+            System.out.println("Hello");
+       
+    }
+}
