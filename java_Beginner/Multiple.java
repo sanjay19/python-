f2a66206f4f77200e555894a29ea414a9d0a1224
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2017-12-10 14:54:05 +0530

    Create Multiple.java

---
+++ java_Beginner/Multiple.java
@@ -0,0 +1,16 @@
+
+
+import java.util.*;
+import java.lang.*;
+public class Multiple {
+    private static int i;
+ public static void main(String[] args) {
+       int n;
+       Scanner sc=new Scanner(System.in);
+       n=sc.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+       System.out.println(n +"*" +i+ "=" +n*i);
+       }
+}
+}
