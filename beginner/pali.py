92b7f0e9acefcdc33f3c72c14adccfb595b3d1ec
NIVI1205 <33159815+NIVI1205@users.noreply.github.com>
2018-01-23 02:23:12 +0000

    Create pali.py

---
+++ beginner/pali.py
@@ -0,0 +1,15 @@
+def main():
+ n=int(input(""))
+ temp=n
+ rev=0
+ while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+ if(temp==rev):
+    print("Yes")
+ else:
+    print("No")
+
+if __name__ == '__main__':
+    main()
